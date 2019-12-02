55815b8d657a9276cb20b0d6a9e13516e861c129
nnistelrooij <47317906+nnistelrooij@users.noreply.github.com>
2019-12-02 15:34:18 +0100

    Update label.py
    Make the x and y coordinate labels integers from 1 up to and including 64. Whether the agents overlap can now also be labeled by clicking on the image when prompted to. Maximized the screen to better label the images. You have to press on the image to give the x and y coordinates. This was done to prevent Nones.

--- label.py
+++ label.py
@@ -1,37 +1,50 @@
 from PIL import Image
 import matplotlib.pyplot as plt
-
 import numpy as np
 
-for i in range(666, 1000):
-    img = np.array(Image.open(f'images/{i}_label.png'))
 
-    blue = (0, 0)
+def label_images(start, end):
+    def show_image(img, title, onclick):
+        plt.imshow(img)
+        fig = plt.gcf()
+        fig.canvas.mpl_connect('button_press_event', onclick)
+        plt.title(title)
+        fig_manager = plt.get_current_fig_manager()
+        fig_manager.window.showMaximized()
+        plt.show()
 
-    def onclick(event):
-        global blue
-        blue = (event.xdata, event.ydata)
-        plt.close()
-
-    plt.imshow(img)
-    fig = plt.gcf()
-    fig.canvas.mpl_connect('button_press_event', onclick)
-    plt.title('Select Blue')
-    plt.show()
-
-    red = (0, 0)
-
-    def onclick(event):
+    def onclickRed(event):
         global red
-        red = (event.xdata, event.ydata)
+        red = (int(event.xdata + 1.5), int(event.ydata + 1.5))
         plt.close()
-    
-    plt.imshow(img)
-    fig = plt.gcf()
-    fig.canvas.mpl_connect('button_press_event', onclick)
-    plt.title('Select Red')
-    plt.show()
 
-    print(i, blue, red)
-    with open('labels.csv', 'a') as f:
-        f.write(f'{i}, {blue[0]}, {blue[1]}, {red[0]}, {red[1]}\n')
+    def onclickBlue(event):
+        global blue
+        blue = (int(event.xdata + 1.5), int(event.ydata + 1.5))
+        plt.close()
+
+    def onclickOverlap(event):
+        global overlap
+        overlap = event.xdata is not None and event.ydata is not None
+        plt.close()
+
+    for i in range(start, end):
+        img = np.array(Image.open(f'data/{i}_label.png'))
+        show_image(img, 'Select Red', onclickRed)
+        show_image(img, 'Select Blue', onclickBlue)
+        show_image(img,
+                   'Click on image to select overlap, '
+                   'click off image to select non-overlap',
+                   onclickOverlap)
+
+        print(i, red, blue, overlap)
+        with open('labels.csv', 'a') as f:
+            f.write(f'{i}, {red[0]}, {red[1]}, {blue[0]}, {blue[1]}, {overlap}\n')
+
+
+if __name__ == '__main__':
+    red = (0, 0)
+    blue = (0, 0)
+    overlap = False
+
+    label_images(0, 166)
