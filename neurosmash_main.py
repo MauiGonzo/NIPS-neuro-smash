16846b09c302c6716a5d3d9933f00fbfb1283e12
Maurice Verbrugge <mauriceverbrugge@MacBook-Pro-van-Maurice.local>
2019-12-06 10:19:22 +0100

    init for agent and environment and pseudocode added

--- neurosmash_main.py
+++ neurosmash_main.py
@@ -4,6 +4,31 @@
 # Niels, Roel, Maurice
 #
 import argparse
+import Neurosmash
+import utils
+
+def init_neurosmash(size=768):
+    ip         = "127.0.0.1" # Ip address that the TCP/IP interface listens to
+    port       = 13000       # Port number that the TCP/IP interface listens to
+    # size       = 768         # Please check the Updates section above for more details
+    timescale  = 1           # Please check the Updates section above for more details
+    agent = Neurosmash.Agent()
+    environment = Neurosmash.Environment()
+    return agent, environment
+
+def pgagent():
+    # do stuff
+    # pgagent, pgenvironment = init_neurosmash()
+    # yxredbluetorchtensor = locations(pgagent, pgenvironment)
+    # pgmetrics = utils.Aggregate(yxredbluetorchtensor)
+    break
+
+def neatagent():
+    # do stuff
+    break
+
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,7 +38,9 @@ if __name__ == '__main__':
     for ag in args.agents:
         if ag == 'PG':
             print('Processing agent: {}'.format(ag))
-            # do stuff
+            # do stuff -> DO run pgagent(), that is
+            # find locations
+            # input for agent is output utils(locations)
         elif ag == 'NEAT':
             print('Processing agent: {}'.format(ag))
             # do stuff
