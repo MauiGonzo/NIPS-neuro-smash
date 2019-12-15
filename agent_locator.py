fe7a60b591a850f2c7549fb9120381747915d70d
Roel Hacking <roel.hacking@gmail.com>
2019-12-15 22:48:59 +0100

    Improved agent locator

--- agent_locator.py
+++ agent_locator.py
@@ -3,10 +3,15 @@ from typing import Optional
 import numpy as np
 from PIL import Image
 from skimage.measure import label, regionprops
-from skimage.morphology import binary_erosion, binary_dilation
+from skimage.morphology import binary_erosion, binary_dilation, dilation
 from skimage.segmentation import watershed
+import os
+from matplotlib import pyplot as plt
+from scipy.ndimage import distance_transform_edt, center_of_mass
+import collections
 
 DEBUG = False
+SAVE_DIFFICULT = False
 if DEBUG:
     logging.basicConfig(level=logging.DEBUG)
 else:
@@ -24,13 +29,15 @@ class Agent(object):
         pos (Optional[np.ndarray], optional): The position of the agent (in pixels). Defaults to None.
         vel (Optional[np.ndarray], optional): The velocity of the agent (in pixels/frame). Defaults to None.
     """    
-    def __init__(self, pos: Optional[np.ndarray] = None, vel: Optional[np.ndarray] = None):
+    def __init__(self, lag: float, pos: Optional[np.ndarray] = None, vel: Optional[np.ndarray] = None):
         """
         Args:
             pos (Optional[np.ndarray], optional): The initial position of the agent. Defaults to None.
             vel (Optional[np.ndarray], optional): The initial velocity of the agent. Defaults to None.
-        """                             
+        """          
+        self.lag = lag                   
         self.pos = pos
+        self.pos_hist = collections.deque(maxlen=40)
         self.computed_pos = pos
         self.vel = vel
 
@@ -45,8 +52,14 @@ class Agent(object):
         """        
         if self.last_pos_update is not None:
             self.vel = (new_pos - self.computed_pos)/(frame - self.last_pos_update)
+            # TODO: Make this threshold configurable
+            if np.linalg.norm(self.vel) > 3:
+                print('oh no')
+                self.update_pos_based_on_vel(frame - self.last_pos_update)
+                return
 
-        self.pos = new_pos
+        self.pos = (new_pos*(1-self.lag) + self.pos*self.lag) if self.pos is not None else new_pos
+        self.pos_hist.append(self.pos)
         self.computed_pos = new_pos
         self.last_pos_update = frame
     
@@ -63,7 +76,9 @@ class Agent(object):
         """        
         if self.vel is None or self.pos is None:
             raise ValueError('Position and/or velocity have not yet been initialized')
-        # self.pos += self.vel*frame_delta
+        vel = (self.pos_hist[-1] - self.pos_hist[0])/(self.pos_hist.maxlen-1)
+        self.pos += vel*frame_delta
+        self.pos_hist.append(self.pos)
 
 class AgentLocator(object):
     """Keeps track of agents based on either their previously recorded velocity or a state image
@@ -73,7 +88,9 @@ class AgentLocator(object):
         red_agent (Agent): The red agent position and velocity
     """     
 
-    def __init__(self, use_dillation: bool = False, use_erosion: bool = False, cooldown_time: int = 5, minimum_agent_area: int = 400, minimum_agent_area_overlap: int = 300, blue_marker_thresh: int = 150, red_marker_thresh: int = 150):
+    def __init__(self, use_dilation: bool = False, use_erosion: bool = False, use_overlap_label_dilation: bool = True, cooldown_time: int = 5, 
+        minimum_agent_area: int = 10, minimum_agent_area_overlap: int = 5, blue_marker_thresh: int = 5, 
+        red_marker_thresh: int = 5, lag: float = 0.16):
         """
         Args:
             cooldown_time (int) -- The time (in frames) between location updates based on the state image, rather than velocity (default: {5})
@@ -81,8 +98,8 @@ class AgentLocator(object):
             blue_marker_thresh (int) -- The number of blue markers required for watershed segmentation (default: {150})
             red_marker_thresh (int) -- The number of red markers required for watershed segmentation (default: {150})
         """
-        self.blue_agent = Agent()
-        self.red_agent = Agent()
+        self.blue_agent = Agent(lag)
+        self.red_agent = Agent(lag)
 
         self.cooldown = 0
         self.cooldown_time = cooldown_time
@@ -91,9 +108,11 @@ class AgentLocator(object):
         self.minimum_agent_area_overlap = minimum_agent_area_overlap
         self.blue_marker_thresh = blue_marker_thresh
         self.red_marker_thresh = red_marker_thresh
-        self.use_dilation = use_dillation
+        self.use_dilation = use_dilation
         self.use_erosion = use_erosion
-
+        self.use_overlap_label_dilation = use_overlap_label_dilation
+        
+        self.errored = False
         self.frame_counter = 0
     
     def update_agent_locations(self, state_img: np.ndarray):
@@ -102,30 +121,34 @@ class AgentLocator(object):
         Args:
             state_img (np.ndarray): The current frame's state image
         """
+        self.errored = False
         try: 
             self.frame_counter += 1
             logging.debug(f'\nFrame {self.frame_counter}')
             if self.cooldown == 0 or self.blue_agent.vel is None or self.red_agent.vel is None:
                 logging.debug('Computing agent locations based on image')
                 foreground = np.mean(np.abs(state_img-background), axis=2)
-                labeled = label(foreground > 0.01 if not self.use_dilation else binary_dilation(foreground > 0.01, selem=np.array([
+                segmented = foreground > 0.01
+                labeled = label(segmented if not self.use_dilation else binary_dilation(segmented, selem=np.array([
                     [0, 1, 0], 
                     [1, 1, 1], 
                     [0, 1, 0]
                 ])))
                 regions = [r for r in regionprops(labeled) if r.area > self.minimum_agent_area]
+                
+                overlap_agents = state_img * np.repeat((labeled == regions[0].label)[:, :, np.newaxis], 3, axis=2)
+
+                index = np.unravel_index(np.argmax(overlap_agents[:, :, 2]-np.mean(overlap_agents[:, :, (0, 1)], axis=2)), overlap_agents[:, :, 2].shape)
+                blue = overlap_agents[index]
+                index = np.unravel_index(np.argmax(overlap_agents[:, :, 0]-np.mean(overlap_agents[:, :, 1:], axis=2)), overlap_agents[:, :, 2].shape)
+                red = overlap_agents[index]
+
                 if len(regions) == 1:
                     logging.debug('Single agent region detected, segmenting...')
-                    overlap_agents = state_img * np.repeat((labeled == regions[0].label)[:, :, np.newaxis], 3, axis=2)
-                    
-                    index = np.unravel_index(np.argmax(overlap_agents[:, :, 2]-np.mean(overlap_agents[:, :, (0, 1)], axis=2)), overlap_agents[:, :, 2].shape)
-                    blue = overlap_agents[index]
-                    index = np.unravel_index(np.argmax(overlap_agents[:, :, 0]-np.mean(overlap_agents[:, :, 1:], axis=2)), overlap_agents[:, :, 2].shape)
-                    red = overlap_agents[index]
 
                     blue_img = np.mean(np.abs(overlap_agents.astype(np.float) - blue), axis=2)
                     red_img = np.mean(np.abs(overlap_agents.astype(np.float) - red), axis=2)
-                    
+
                     markers = np.zeros_like(blue_img)
                     for thr in np.linspace(0, 50, 100):
                         markers[binary_erosion(blue_img < thr) if self.use_erosion else blue_img < thr] = 1
@@ -136,12 +159,10 @@ class AgentLocator(object):
                         if np.sum(markers == 2) > self.red_marker_thresh:
                             break
 
-                    labeled = watershed(np.mean(overlap_agents, axis=2), markers, mask=(labeled == regions[0].label))
-                    regions = regionprops(labeled)
-                    print(len(regions))
-                    regions = [r for r in regions if r.area > self.minimum_agent_area_overlap]
-                    
-                    if len(regions) != 2:
+                    labeled = watershed(-distance_transform_edt(segmented), markers=markers, mask=segmented)
+
+                    if np.sum(labeled == 1) == 1 or np.sum(labeled == 2) == 0:
+                        self.errored = True
                         if DEBUG:
                             Image.fromarray(markers.astype(np.uint8)).save('error_markers.png')
                             raise ValueError(f'Found {len(regions)} regions, must be 2')
@@ -149,27 +170,48 @@ class AgentLocator(object):
                             logging.warning('Failed to segment double agent region, using velocity instead')
                             self._update_agent_locations_vel()
                             return
-                
-                if np.argmax(np.mean(state_img[labeled == regions[1].label], axis=0)) == 0:
-                    blue_region = regions[0]
-                    red_region = regions[1]
-                else:
-                    blue_region = regions[1]
-                    red_region = regions[0]
 
-                self.blue_agent.update_pos(np.array(blue_region.centroid), self.frame_counter)
-                self.red_agent.update_pos(np.array(red_region.centroid), self.frame_counter)
+                    if (np.min(np.mean(np.abs((overlap_agents * np.repeat((labeled == 2)[:, :, np.newaxis], 3, axis=2)).astype(np.float) - red), axis=2)) <
+                            np.min(np.mean(np.abs((overlap_agents * np.repeat((labeled == 2)[:, :, np.newaxis], 3, axis=2)).astype(np.float) - blue), axis=2))):
+                        self.blue_agent.update_pos(np.array(center_of_mass(labeled == 1)), self.frame_counter)
+                        self.red_agent.update_pos(np.array(center_of_mass(labeled == 2)), self.frame_counter)
+                    else:
+                        self.blue_agent.update_pos(np.array(center_of_mass(labeled == 2)), self.frame_counter)
+                        self.red_agent.update_pos(np.array(center_of_mass(labeled == 1)), self.frame_counter)
+                else:
+                    if np.argmax(np.mean(state_img[labeled == regions[1].label], axis=0)) == 0:
+                        blue_region = regions[0]
+                        red_region = regions[1]
+                    else:
+                        blue_region = regions[1]
+                        red_region = regions[0]
+
+                    self.blue_agent.update_pos(np.array(blue_region.centroid), self.frame_counter)
+                    self.red_agent.update_pos(np.array(red_region.centroid), self.frame_counter)
 
                 self.cooldown = self.cooldown_time
             else:
                 self._update_agent_locations_vel()
                 self.cooldown -= 1
         except:
-            Image.fromarray(labeled.astype(np.uint8)).save('error_labeled.png')
-            Image.fromarray(state_img.astype(np.uint8)).save('error.png')
-            raise ValueError()
+            self.errored = True
+            self._save_difficult(state_img, labeled, None)
+            if DEBUG:
+                Image.fromarray(labeled.astype(np.uint8)).save('error_labeled.png')
+                Image.fromarray(state_img.astype(np.uint8)).save('error.png')
+                raise ValueError()
+            else:
+                self._update_agent_locations_vel()
     
     def _update_agent_locations_vel(self):
         logging.debug('Updating agent locations based on velocity')
         self.blue_agent.update_pos_based_on_vel(1)
         self.red_agent.update_pos_based_on_vel(1)
+    
+    def _save_difficult(self, state_img, labeled, markers):
+        if SAVE_DIFFICULT and np.random.random() < 0.009:
+            i = len([f for f in os.scandir('difficult_states') if not (f.name.endswith('labeled.png') or f.name.endswith('markers.png'))])
+            if markers is not None:
+                Image.fromarray(markers.astype(np.uint8)).save(f'difficult_states/{i}_markers.png')
+            Image.fromarray(labeled.astype(np.uint8)).save(f'difficult_states/{i}_labeled.png')
+            Image.fromarray(state_img.astype(np.uint8)).save(f'difficult_states/{i}.png')
--- neurosmash_main.py
+++ neurosmash_main.py
@@ -10,154 +10,138 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import Neurosmash
+import utils
+import platform
+import subprocess
+from neat_agent import NeatAgent
+import matplotlib.pyplot as plt
 from agent_locator import AgentLocator
-from pg_agent import PGAgent
-from q_agent import QAgent
-from utils import Aggregator
+from typing import List, Dict, Type
+import numpy as np
+import logging
+import sys
 
-def neatagent():
+ip         = "127.0.0.1" # Ip address that the TCP/IP interface listens to
+port       = 13000       # Port number that the TCP/IP interface listens to
+size       = 64         # Please check the Updates section above for more details
+timescale  = 1           # Please check the Updates section above for more details
+
+def init_neurosmash(size=768):
+    agent = Neurosmash.Agent()
+    environment = Neurosmash.Environment(size=size)
+    return agent, environment
+
+def pgagent():
     # do stuff
+    # pgagent, pgenvironment = init_neurosmash()
+    # yxredbluetorchtensor = locations(pgagent, pgenvironment)
+    # pgmetrics = utils.Aggregate(yxredbluetorchtensor)
     pass
 
+# def neatagent():
+#     # do stuff
+#     pass
 
-def get_locations(state_img: list):
-    """Determines the agents' locations in pixel coordinates
-       given the state image.
+class NeurosmashRunner(object):
+    def __init__(self, args):
+        self.args = args
+    
+    def run_round(self, agent: Neurosmash.Agent):
+        print('aaaaah')
+        end, reward, state = env.reset()
+        print('aaah2')
+        state_img = state_to_ndarray(state)
 
-    Args:
-        state_img = [[int]] current state of the environment
+        al = AgentLocator(cooldown_time=1, minimum_agent_area=10, minimum_agent_area_overlap=4, blue_marker_thresh=2, red_marker_thresh=2)
+        al.update_agent_locations(state_img)
+        blue, red = al.blue_agent.pos, al.red_agent.pos
+        if self.args.b_plot:
+            mpl_img = plt.imshow(state_img)
+            ax = plt.gca()
+            mpl_blue = ax.scatter(blue[1], blue[0], c='b')
+            mpl_red = ax.scatter(red[1], red[0], c='r')
+            plt.ion()
+        
+        steps = 0
+        while end == 0 and steps < 460:
+            steps += 1
+            action = agent.step(end, reward, state, blue, red)
+            end, reward, state = env.step(action)
+            # print(end, reward)
+            state_img = state_to_ndarray(state)
+            al.update_agent_locations(state_img)
+            blue, red = al.blue_agent.pos, al.red_agent.pos
+            if args.b_plot:
+                mpl_img.set_data(state_img)
+                mpl_blue.set_offsets(blue[::-1])
+                mpl_red.set_offsets(red[::-1])
+                plt.draw()
+                plt.pause(0.0001)
+        print(steps, reward > 0.1)
+        return steps, reward > 0.1
+        
 
-    Returns [((x_red, y_red), (x_blue, y_blue))]:
-        The x and y pixel coordinate locations of the red and blue agents.
-    """
-    # get (size, size, 3) np.ndarray of current state
-    state_img = np.asarray(environment.state2image(state_img), np.uint8, 'f')
+def state_to_ndarray(state: List) -> np.ndarray:
+    return np.array(state).reshape(size, size, 3)
 
-    # update agents' locations and retrieve said locations
-    locator.update_agent_locations(state_img)
-    loc_red = locator.red_agent.pos
-    loc_blue = locator.blue_agent.pos
-
-    return loc_red, loc_blue
-
-
-def plot_locations(state_img, xy_red, xy_blue):
-    """Plots the given locations of the agents on the state image.
-
-    Args:
-        state_img = [[int]] current state of the environment
-        xy_red    = [(int,int)] x and y pixel coordinates of red agent
-        xy_blue   = [(int,int)] x and y pixel coordinates of blue agent
-    """
-    # get PIL image of current state
-    state_img = environment.state2image(state_img)
-
-    # plot agents' locations
-    ax.clear()
-    ax.imshow(state_img, extent=(0.5, size + 0.5, size + 0.5, 0.5))
-    ax.scatter([xy_red[0], xy_blue[0]], [xy_red[1], xy_blue[1]], c=['r', 'b'])
-    ax.set_title('Predicted locations of agents')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_xlim(0, size + 1)
-    ax.set_ylim(size + 1, 0)
-    fig.canvas.draw()
-    plt.pause(0.0001)
-
-
-def run_agent(env, agent, epsilon_start=0.95, epsilon_decay=0.995,
-              epsilon_min=0.005, n_episodes=1000, train=True):
-    """Function to run the agent for a given number of episodes.
-
-    Args:
-        env           = [Neurosmash.Environment] the environment in which
-                                                 the agent lives and acts
-        agent         = [Neurosmash.Agent] the agent that determines the
-                                           action, given the current state
-        epsilon_start = [float] starting value for exploration/
-                                exploitation parameter
-        epsilon_decay = [float] number signifying how fast epsilon decreases
-        epsilon_min   = [float] minimal value for the epsilon parameter
-        n_episodes    = [int] number of episodes the agent will learn for
-        train         = [bool] whether to update the agent's model's parameters
-    """
-    R = []  # keep track of rewards
-    epsilon = epsilon_start  # initialize epsilon
-    for i_episode in range(n_episodes):  # loop over episodes
-        # add element to rewards list
-        R.append(0)
-
-        # reset environment, create first observation
-        end, reward, state_img = env.reset()
-        old_loc_red, old_loc_blue = get_locations(state_img)
-        old_state = aggregator.aggregate(old_loc_red, old_loc_red,
-                                         old_loc_blue, old_loc_blue)
-        while not end:
-            # choose an action
-            if random.random() < epsilon:
-                # random actions to explore environment (exploration)
-                action = random.randrange(environment.n_actions)
-            else:
-                # strictly follow currently learned behaviour (exploitation)
-                action = agent.step(end, reward, old_state)
-
-            for _ in range(locator.cooldown_time + 1):
-                # do action, get reward and a new observation for the next round
-                end, reward, state_img = env.step(action)
-                new_loc_red, new_loc_blue = get_locations(state_img)
-                plot_locations(state_img, new_loc_red, new_loc_blue)
-
-            new_state = aggregator.aggregate(old_loc_red, new_loc_red,
-                                             old_loc_blue, new_loc_blue)
-
-            if train:  # adjust agent model's parameters (training step)
-                agent.train(end, action, old_state, reward, new_state)
-
-            # update state and locations variables
-            old_state = new_state
-            old_loc_red, old_loc_blue = new_loc_red, new_loc_blue
-
-            # track the rewards
-            R[i_episode] += reward
-
-            # decay epsilon parameter
-            epsilon = max(epsilon_min, epsilon * epsilon_decay)
-
+agents_types: Dict[str, Type[Neurosmash.Agent]] = {
+    'NEAT': NeatAgent, 
+    'RAND': Neurosmash.Agent
+}
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('agents', nargs='+', type=str, help='agent names to train, choose <PG, NEAT, Q, random>')
+    parser.add_argument('agent', type=str, choices=list(agents_types.keys()), help='agent names to train, choose <PG, NEAT>')
+    parser.add_argument('--plot-positions', dest='b_plot', action='store_true', help='whether the detected agent positions should be plotted')
     args = parser.parse_args()
+    
+    if platform.system() == 'Darwin':
+        p = subprocess.Popen(['open', 'Mac.app'], stderr=subprocess.PIPE)
+        while p.poll() is None:
+            pass
 
-    size = 64
-    locator = AgentLocator(cooldown_time=1,
-                           minimum_agent_area=10,
-                           minimum_agent_area_overlap=4,
-                           blue_marker_thresh=2,
-                           red_marker_thresh=2)
-    aggregator = Aggregator(size, 0.02*(locator.cooldown_time + 1))
-    environment = Neurosmash.Environment(size=size, timescale=10)
+    connected = False
+    while not connected:
+        # if platform.system() in ['Darwin'] and p.poll() is None:
+        #     logging.critical('Failed to run Neurosmash')
+        #     sys.exit(1)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    fig.show()
-    fig.canvas.draw()
+        try:
+            env = Neurosmash.Environment(size=size, timescale=10)
+            connected = True
+        except ConnectionRefusedError:
+            pass
+    
+    runner = NeurosmashRunner(args)
+    agent = agents_types[args.agent](runner)
+    agent.run()
 
-    for ag in args.agents:
-        if ag == 'PG':
-            print('Processing agent: {}'.format(ag))
-            pg_agent = PGAgent(aggregator.n_obs, environment.n_actions)
-            run_agent(environment, pg_agent)
-        elif ag == 'NEAT':
-            print('Processing agent: {}'.format(ag))
-            # do stuff
-        elif ag == 'Q':
-            print('Processing agent: {}'.format(ag))
-            q_agent = QAgent(aggregator.n_obs, environment.n_actions)
-            run_agent(environment, q_agent)
-        elif ag == 'random':
-            print('Processing agent: {}'.format(ag))
-            random_agent = Neurosmash.Agent()
-            run_agent(environment, random_agent)
-        else: # current agent not known
-            print('unknonw agent   : {}'.format(ag))
+
+    # end, reward, state = env.reset()
+    # state_img = state_to_ndarray(state)
+    # # blue, red = detect_positions(state_img, None, None)
+    # al = AgentLocator()
+    # al.update_agent_locations(state_img)
+    # blue, red = al.blue_agent.pos, al.red_agent.pos
+    # if args.b_plot:
+    #     mpl_img = plt.imshow(state_img)
+    #     ax = plt.gca()
+    #     mpl_blue = ax.scatter(blue[1], blue[0], c='b')
+    #     mpl_red = ax.scatter(red[1], red[0], c='r')
+    #     plt.ion()
+
+    # while not agent.is_finished():
+    #     action = agent.step(end, reward, state)
+    #     end, reward, state = env.step(action)
+    #     state_img = state_to_ndarray(state)
+    #     al.update_agent_locations(state_img)
+    #     blue, red = al.blue_agent.pos, al.red_agent.pos
+    #     if args.b_plot:
+    #         mpl_img.set_data(state_img)
+    #         mpl_blue.set_offsets(blue[::-1])
+    #         mpl_red.set_offsets(red[::-1])
+    #         plt.draw()
+    #         plt.pause(0.0001)
+
+    if platform.system() in ['Darwin']:
+        p.terminate()
